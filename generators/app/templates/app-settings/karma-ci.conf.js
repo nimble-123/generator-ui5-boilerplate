c8804188c3d4d7bcaefddab57a723145171f7fd5
Nils Lutz <info@nilslutz.de>
2019-03-17 14:35:26 +0100

    chore(templates): update ui5-tooling
    BREAKING CHANGES: Update ui5-tooling

--- generators/app/templates/app-settings/karma-ci.conf.js
+++ generators/app/templates/app-settings/karma-ci.conf.js
@@ -1,55 +1,37 @@
-// Karma configuration
-// Generated on Wed Jun 13 2018 14:38:44 GMT+0200 (CEST)
-
 module.exports = function(config) {
-	require("./karma.conf")(config);
-	config.set({
+    'use strict';
 
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-		preprocessors: {
-			'{webapp,webapp/!(test)}/*.js': ['coverage']
-		},
+    require('./karma.conf')(config);
+    config.set({
+        preprocessors: {
+            '{webapp,webapp/!(test)}/*.js': ['coverage'],
+        },
 
-		coverageReporter: {
-			includeAllSources: true,
-			reporters: [
-				{
-					type: 'html',
-					dir: '../coverage/'
-				},
-				{
-					type: 'text'
-				}
-			],
-			check: {
-				each: {
-					statements: 100,
-					branches: 100,
-					functions: 100,
-					lines: 100
-				}
-			}
-		},
+        coverageReporter: {
+            includeAllSources: true,
+            reporters: [
+                {
+                    type: 'html',
+                    dir: 'coverage',
+                },
+                {
+                    type: 'text',
+                },
+            ],
+            check: {
+                each: {
+                    statements: 100,
+                    branches: 100,
+                    functions: 100,
+                    lines: 100,
+                },
+            },
+        },
 
-		client: {
-			qunit: {
-				showUI: false
-			}
-		},
+        reporters: ['progress', 'coverage'],
 
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'coverage'],
+        browsers: ['ChromeHeadless'],
 
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['PhantomJS'],
-
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: true,
-
-	});
+        singleRun: true,
+    });
 };
